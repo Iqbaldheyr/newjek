bfe23385a19e0e525bbbe7e7e4bc3193d47d6356
Iqbaldheyr <64371886+Iqbaldheyr@users.noreply.github.com>
2022-02-19 12:30:52 +0700

    Update newjek.php

--- newjek.php
+++ newjek.php
@@ -108,8 +108,8 @@ function change(){
          $pilih1 = trim(fgets(STDIN));
          if($pilih1 == "y" || $pilih1 == "Y"){
          //if($pilih1 == "y" && strpos($no, "628")){
-         echo color("red","========( PIN ANDA = 225588 )========")."\n";
-         $data2 = '{"pin":"225588"}';
+         echo color("red","========( PIN ANDA = 141219 )========")."\n";
+         $data2 = '{"pin":"141219"}';
          $getotpsetpin = request("/wallet/pin", $token, $data2, null, null, $uuid);
          echo "Otp set pin: ";
          $otpsetpin = trim(fgets(STDIN));
